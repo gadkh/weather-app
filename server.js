653cd07b4279af3d2a5252c515b22369d72d62ad
Gad <gadkh2@gmail.com>
2019-10-16 15:46:17 +0300

    deploy

--- server.js
+++ server.js
@@ -1,30 +1,13 @@
-
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-// });
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
 const express = require('express');
 const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/weather-app'));
+app.use(express.static(__dirname + '/dist/weather-app'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/weather-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/weather-app/index.html'));
 });
 
 // default Heroku PORT
