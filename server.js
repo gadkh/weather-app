a6dec97fef26009a1cd35b32d6f846417b9e1dba
Gad <gadkh2@gmail.com>
2019-10-16 15:06:16 +0300

    change directory in server.js

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/weather-app'));
+app.use(express.static(__dirname + '/weather-app'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/weather-app/index.html'));
+  res.sendFile(path.join(__dirname + '/weather-app/index.html'));
 });
 
 // default Heroku PORT
